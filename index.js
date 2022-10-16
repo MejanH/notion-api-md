1491fc7d8cd3f5c25788aa3a29b6d7dda9934447
MejanH <muhammad.mejanul.haque@gmai.com>
2022-10-17 03:49:45 +0600

    added new testing code

--- index.js
+++ index.js
@@ -16,10 +16,37 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 
 
 // retriever database to fetch all pages(blog posts)
+(async () => {
+  // retrieve page
+    // const listUsersResponse = await notion.pages.retrieve({page_id: '7bb4e270-9f5f-49cc-b23e-4d9c263b510b'})
+  // retrieve all blocks of a page
+    // const listUsersResponse = await notion.blocks.children.list({block_id: '7bb4e270-9f5f-49cc-b23e-4d9c263b510b'})
+  // retrieve all columns of a column_list block
+    // const listUsersResponse = await notion.blocks.children.list({block_id: 'eb9d560e-ec93-4cf3-8b03-46f0c35f6a8e'})
+  // retrieve column children of a column block
+    // const listUsersResponse = await notion.blocks.children.list({block_id: 'a09e62e0-c893-42a6-8213-5e86978bf542'})
+
+    // const listUsersResponse = await notion.databases.query({database_id: '7e0133768f3244e29a20dcd25eb312e3'})
+    // console.log(console.dir(listUsersResponse, {depth: null}))
+  })()
+
+// unsupported notion block type
+
 // (async () => {
-//     const listUsersResponse = await notion.databases.query({database_id: 'c6a0f57829ed4ed3813105b2ac000791'})
-//     console.log(console.dir(listUsersResponse, {depth: null}))
-//   })()
+//   // n2m.setCustomTransformer('column_list', async (block) => {
+//   //   const columnBlocks = await n2m.pageToMarkdown(block.id)
+//   //   const columnString = n2m.toMarkdownString(columnBlocks)
+//   //   return columnString
+//   // })
+//     console.log('ran below')
+//   const mdblocks = await n2m.pageToMarkdown("7bb4e2709f5f49ccb23e4d9c263b510b");
+//   const mdString = n2m.toMarkdownString(mdblocks);
+
+//   //writing to file
+//   fs.writeFile("test.md", mdString, (err) => {
+//     console.log(err);
+//   });
+// })();
 
 // download images and generater markdown files
 
